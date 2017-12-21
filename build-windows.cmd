d42ab2a0b027f1e9e88cef5b233a900acf885a6d
Alex Shafer <shafer.alex@gmail.com>
2017-12-21 18:34:37 -0500

    Optimize build output on windows.

--- build-windows.cmd
+++ build-windows.cmd
@@ -1,6 +1,6 @@
 CALL .\.venv\Scripts\activate
 pip install -r .\requirements.txt
-pyinstaller .\rocketleagueminimapgenerator\main.py --onefile
+set PYTHONOPTIMIZE=1 && pyinstaller .\rocketleagueminimapgenerator\main.py --onefile
 
 MOVE /Y .\dist\main.exe .\dist\rocketleagueminimapgenerator.exe
 COPY /Y .\README.md .\dist\README.txt
